a2202ad8778de20eb2b1af58c9febb7f3def8505
luke <l.j.dwsn@gmail.com>
2019-12-26 03:53:33 +1100

    Please review my changes to posting beers server side

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { beerService } from "./services";
 import { connectToDatabase } from "./utils/db";
 import { PORT } from "./utils/constants";
@@ -6,11 +7,19 @@ import { PORT } from "./utils/constants";
 const app = express();
 
 app.use(express.static("static"));
+app.use(bodyParser.json());
 
 app.get("/beers", async (req, res) => {
   const beers = await beerService.getBeers();
   res.send({
     beers
+  });
+});
+
+app.post("/beers", async (req, res) => {
+  await beerService.createBeer(req.body);
+  res.send({
+    status: "Beer was saved! YUM"
   });
 });
 
