e3d2143506da96716e2a9cbd1abde0de5b333f8f
Sean Dawson <contact@seandawson.info>
2019-12-26 12:13:19 +1100

    Add bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./state/store";
-import "./style/index.css";
 import Home from "./pages/home";
 
 ReactDOM.render(
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,18 +1,23 @@
 import React from "react";
 import logo from "../images/logo.svg";
 import "../style/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import BeerList from "../components/beer-list";
 
 const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Beer List</h1>
-      </header>
-      <main className="container">
-        <BeerList />
-      </main>
+    <div className="container">
+      <div className="row">
+        <header className="col-12">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Beer List</h1>
+        </header>
+        <div className="row">
+          <main className="col-12">
+            <BeerList />
+          </main>
+        </div>
+      </div>
     </div>
   );
 };
